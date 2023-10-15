52fe310c2e0b94b6f101f301d49dcfb53f211879
VERA-OBIERO <vera.obiero@gmail.com>
2023-10-15 20:35:50 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    toDoInput(e.target.newTaskDescription.value);
+
+  })
+
+
 });
+
